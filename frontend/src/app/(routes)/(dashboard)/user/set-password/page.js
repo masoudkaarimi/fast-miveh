6db171f6547e6cfaef560715c0ecfca2e7729215
masoudkaarimi <masoud.develop@gmail.com>
2025-07-19 22:12:29 +0330

    Bug fixed

--- frontend/src/app/(routes)/(dashboard)/user/set-password/page.js
+++ frontend/src/app/(routes)/(dashboard)/user/set-password/page.js
@@ -1,6 +1,32 @@
+"use client";
+
 import {PasswordSetForm} from "@/features/auth/components/PasswordSetForm";
+import {useUserProfile} from "@/features/user/profile/hooks/useProfile";
+import {useRouter} from "next/navigation";
+import {useEffect} from "react";
+import {FullPageSpinner} from "@/components/shared/FullPageSpinner";
 
 export default function SetPasswordPage() {
+    // --- Hooks ---
+    const router = useRouter();
+    const {data: userData, isLoading} = useUserProfile();
+    const user = userData?.data ?? null;
+
+    useEffect(() => {
+        if (isLoading || !user) return; // Wait until user data is loaded
+
+        // If the user is loading or already has a password, redirect them to the user dashboard
+        if (!isLoading && user && user.has_password) {
+            router.push('/user');
+        }
+    }, [isLoading, user, router]);
+
+    // Show a spinner while checking the user status
+    if (isLoading || (user && user.has_password)) {
+        return <FullPageSpinner/>;
+    }
+
+    // If the user has no password, show the set password form
     return (
         <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
             <div className="w-full max-w-sm">
--- frontend/src/app/(routes)/(dashboard)/user/welcome/page.js
+++ frontend/src/app/(routes)/(dashboard)/user/welcome/page.js
@@ -10,21 +10,24 @@ import {ProfileCompletionForm} from "@/features/auth/components/ProfileCompletio
 export default function WelcomePage() {
     // --- Hooks ---
     const router = useRouter();
-    const {data: user, isLoading} = useUserProfile();
+    const {data: userData, isLoading} = useUserProfile();
+    const user = userData?.data ?? null;
 
     useEffect(() => {
-        // If the user is loading or already has a complete profile, redirect them to the user dashboard
+        if (isLoading || !user) return; // Wait until user data is loaded
+
+        // If the user is loading or has a complete profile, redirect them to the user dashboard
         if (!isLoading && user && user.is_profile_complete) {
             router.push('/user');
         }
     }, [isLoading, user, router]);
 
-    // Show a spinner while checking the profile status
+    // Show a spinner while checking the user status
     if (isLoading || (user && user.is_profile_complete)) {
         return <FullPageSpinner/>;
     }
 
-    // If the profile is not complete, show the form
+    // If the user has an incomplete profile, show the profile completion form
     return (
         <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
             <div className="w-full max-w-sm">
